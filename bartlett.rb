f7fb3f0dd8aa5920a12a321241c1b6c816c4a312
Fernando Freire <fernando.freire@nike.com>
2017-04-11 10:17:07 -0700

    Update bartlett to 1.5.1

--- bartlett.rb
+++ bartlett.rb
@@ -2,8 +2,8 @@ class Bartlett < Formula
 
   desc "A simple Jenkins command line client to serve your needs."
   homepage "https://github.com/Nike-Inc/bartlett"
-  url "https://github.com/Nike-Inc/bartlett/releases/download/1.4.0/bartlett-static-1.4.0.tar.gz"
-  sha256 "d5034321dc5c5eef5f799c55e25c43d48a7b8460956e0014cb98db83e37c132a"
+  url "https://github.com/Nike-Inc/bartlett/releases/download/1.5.1/bartlett-static-1.5.1.tar.gz"
+  sha256 "3d46b4813914df07071bf2314d8e8df09a5c849c80827f33a16174de6cad1d50"
 
   def install
     bin.install "bartlett"
