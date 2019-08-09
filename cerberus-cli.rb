9c76c55e4bc2290d3a38d61077876afb7d257b22
dzha52 <david.zhao@nike.com>
2019-08-09 11:50:46 -0700

    chore: update cerberus-cli to v0.10.2

--- cerberus-cli.rb
+++ cerberus-cli.rb
@@ -1,13 +1,13 @@
 class CerberusCli < Formula
   desc "A CLI for the Cerberus API."
   homepage "http://engineering.nike.com/cerberus"
-  url "https://github.com/Nike-Inc/cerberus-cli/releases/download/v0.8.0/cerberus-cli-darwin-amd64"
-  sha256 "f9e4fd2e7c008322069a0011882511c8cbddc37a3b6381752d05f099c2961373"
-  version "v0.8.0"
+  url "https://github.com/Nike-Inc/cerberus-cli/releases/download/v0.10.2/cerberus-cli-darwin-amd64"
+  sha256 "32a715111f9dda1da5a014dcca6c10fea32199e75b3ad810622556b96ffafc1c"
+  version "v0.10.2"
 
   resource "additional_files" do
-    url "https://github.com/Nike-Inc/cerberus-cli/archive/v0.8.0.tar.gz"
-    sha256 "c8ae0234600021fa4c60a2a9be7fe8c149315dd32f8b452f9a078d651a1398b9"
+    url "https://github.com/Nike-Inc/cerberus-cli/archive/v0.10.2.tar.gz"
+    sha256 "6dd9ded54087d787eacf82977944ffb2f13d7d250526d9ab8e0f976054336f85"
   end
 
   option "with-completion"
