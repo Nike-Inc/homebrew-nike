7949ebd6ab20f3bd8547347ba2550a0d3822c049
Todd Underwood <todd.underwood@nike.com>
2020-09-01 13:35:35 -0700

    feat: Updates the Cerberus CLI version to enable the AWS China partition

--- cerberus-cli.rb
+++ cerberus-cli.rb
@@ -1,13 +1,13 @@
 class CerberusCli < Formula
   desc "A CLI for the Cerberus API."
   homepage "http://engineering.nike.com/cerberus"
-  url "https://github.com/Nike-Inc/cerberus-cli/releases/download/v0.11.1/cerberus-cli-darwin-amd64"
-  sha256 "c81225a56f20cd5520c262d775c0395bdcb073bd4a2b82d1055a1eb19b1269c6"
-  version "v0.11.1"
+  url "https://github.com/Nike-Inc/cerberus-cli/releases/download/v0.12.0/cerberus-cli-darwin-amd64"
+  sha256 "a3b3e263c4ebbfec9ea1e8ad4bbe914fd7b1cee8541fb5740d63c466d45220c6"
+  version "v0.12.0"
 
   resource "additional_files" do
-    url "https://github.com/Nike-Inc/cerberus-cli/archive/v0.11.1.tar.gz"
-    sha256 "5f64b17c98afd850b1309eec130923983030b235bdbfc906693bd6a586c18a7d"
+    url "https://github.com/Nike-Inc/cerberus-cli/archive/v0.12.0.tar.gz"
+    sha256 "2d51db82115dfa29957171dc678f76cdce4ef8258ea4ca2311fa06b83280bd91"
   end
 
   option "with-completion"
