d42195b5e033ac27bf5c4600f659ca75f45aba7c
Sam Lichlyter <sam.lichlyter@nike.com>
2021-11-15 12:22:36 -0800

    fix cerberus version to be 1.0.3

--- cerberus-cli.rb
+++ cerberus-cli.rb
@@ -3,7 +3,7 @@ class CerberusCli < Formula
   homepage "http://engineering.nike.com/cerberus"
   url "https://github.com/Nike-Inc/cerberus-cli/releases/download/v1.0.3/cerberus-cli-darwin-amd64"
   sha256 "cdabb68f8040a41fa54328f084b529baaa04fc4044c1eac7ffdf24f0a387cd4e"
-  version "v1.0.1"
+  version "v1.0.3"
 
   resource "additional_files" do
     url "https://github.com/Nike-Inc/cerberus-cli/archive/v1.0.3.tar.gz"
